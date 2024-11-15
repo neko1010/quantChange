1b2903ad0072a7ed1ea3f05269f40b562074465b
Nick Kolarik <nicholaskolarik@u.boisestate.edu>
2024-11-15 14:16:09 -0700

    include multivariate bcp

--- quantChangeALL.R
+++ quantChangeALL.R
@@ -61,9 +61,20 @@ plot(data.ts.NA)
 fit = bfast(data.ts, h = 0.15, season = "dummy")
 #fit.na = bfast(data.ts.NA, h = 0.15, season = "dummy") ## can't handle missing data
 
+## test the sensitivity to the prop of observations used to detect breakpoint; < 0.5
+fit.05 = bfast(data.ts, h = 0.05, season = "dummy")
+fit.25 = bfast(data.ts, h = 0.25, season = "dummy")
+fit.35 = bfast(data.ts, h = 0.35, season = "dummy")
+fit.45 = bfast(data.ts, h = 0.45, season = "dummy")
+
 ## plot the output 
 plot(fit, ANOVA = T)
-#plot(fit.NA, ANOVA = T)
+
+#plot(fit.05, ANOVA = T) ## no diffs
+#plot(fit.25, ANOVA = T) ## no diffs
+#plot(fit.35, ANOVA = T) ## no diffs
+#plot(fit.45, ANOVA = T) ## no diffs
+#plot(fit.NA, ANOVA = T) ## doesn't work
 
 ######### Bayesian Change Point (for comparison) #############
 
@@ -78,35 +89,81 @@ data.fill$year = as.numeric(format(as.Date(data.fill$system.time_start, format =
 results.bcp = bcp(data.fill$mesic)
 plot(results.bcp,main = "WRP Mesic Probabilities of Change - Yankee Fork",xlab = "Date", xaxlab = data.fill$year )
 
-## with NAs  - DOESNT WORK
-#results.bcp.na = bcp(data.NAs$mesic)
-#plot(results.bcp,main = "WRP Mesic Probabilities of Change - Yankee Fork",xlab = "Date", xaxlab = data.NAs$date )
+summary(results.bcp)
 
-
-## NEXT STEPS - MULTIVARIATE - control for months as random effects, climate (drought) as fixed effect
-
-#install.packages('Rbeast')
-library(Rbeast)
-
-results.beast = beast(data.fill$mesic, start = as.Date('2004-6-1'), deltat = "3 months") ##not quite right - missing 2016 data.... 
-plot(results.beast, interactive = F)
-
-## missing data
-results.beast.na = beast(data.ts.NA) ## supposed to work but doesnt... https://cran.r-project.org/web/packages/Rbeast/Rbeast.pdf
-results.beast.na = beast(data.ts.NA, start = c('2004, 1, 15'), deltat = 1/4, period = 365 )  
-plot(results.beast.na, interactive = F)
-
-## BSTS 
-library(CausalImpact)
+## Multivariate - use PDSI as a predictor with time
 
 ## data with drought index predictor
 pdsi = read.csv('pdsi_Yankee.csv')
 
 ## remove 2016 to have corresponding values with the time series
 pdsi$year = as.numeric(format(as.Date(pdsi$system.time_start, format = "%b %e, %Y"), "%Y"))
-pdsi = subset(pdsi, year != 2016)
+pdsi.no2016 = subset(pdsi, year != 2016)
 
-data.bsts=  cbind(data.fill$mesic, pdsi$pdsi)
+data.multiv = cbind(data.fill, pdsi.no2016$pdsi)
+results.bcp.multiv = bcp(data.multiv$mesic, data.multiv$`pdsi.no2016$pdsi`)
+plot(results.bcp.multiv,main = "WRP Mesic Probabilities of Change - Yankee Fork Multivariate",xlab = "Date", xaxlab = data.fill$year )
+
+summary(results.bcp.multiv)
+ 
+## with NAs  - DOESNT WORK
+#results.bcp.na = bcp(data.NAs$mesic)
+#plot(results.bcp,main = "WRP Mesic Probabilities of Change - Yankee Fork",xlab = "Date", xaxlab = data.NAs$date )
+
+## Test sensitivity of the w0 and p0 params - quant diffs minimal, no qualitative diffs
+#results.bcp.w1 = bcp(data.fill$mesic, w0 = 0.1)
+#results.bcp.w3 = bcp(data.fill$mesic, w0 = 0.3)
+#results.bcp.w4 = bcp(data.fill$mesic, w0 = 0.4)
+#results.bcp.w5 = bcp(data.fill$mesic, w0 = 0.5)
+#results.bcp.w6 = bcp(data.fill$mesic, w0 = 0.6)
+#results.bcp.w7 = bcp(data.fill$mesic, w0 = 0.7)
+#results.bcp.w8 = bcp(data.fill$mesic, w0 = 0.8)
+#results.bcp.w9 = bcp(data.fill$mesic, w0 = 0.9)
+#
+#results.bcp.p1 = bcp(data.fill$mesic, p0 = 0.1)
+#results.bcp.p3 = bcp(data.fill$mesic, p0 = 0.3)
+#results.bcp.p4 = bcp(data.fill$mesic, p0 = 0.4)
+#results.bcp.p5 = bcp(data.fill$mesic, p0 = 0.5)
+#results.bcp.p6 = bcp(data.fill$mesic, p0 = 0.6)
+#results.bcp.p7 = bcp(data.fill$mesic, p0 = 0.7)
+#results.bcp.p8 = bcp(data.fill$mesic, p0 = 0.8)
+#results.bcp.p9 = bcp(data.fill$mesic, p0 = 0.9)
+#
+#plot(results.bcp.w1, main = "WRP Mesic Probabilities of Change - w0 = 0.1",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.w3, main = "WRP Mesic Probabilities of Change - w0 = 0.3",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.w4, main = "WRP Mesic Probabilities of Change - w0 = 0.4",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.w5, main = "WRP Mesic Probabilities of Change - w0 = 0.5",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.w6, main = "WRP Mesic Probabilities of Change - w0 = 0.6",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.w7, main = "WRP Mesic Probabilities of Change - w0 = 0.7",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.w8, main = "WRP Mesic Probabilities of Change - w0 = 0.8",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.w9, main = "WRP Mesic Probabilities of Change - w0 = 0.9",xlab = "Date", xaxlab = data.fill$year )
+#
+#plot(results.bcp.p1, main = "WRP Mesic Probabilities of Change - p0 = 0.1",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.p3, main = "WRP Mesic Probabilities of Change - p0 = 0.3",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.p4, main = "WRP Mesic Probabilities of Change - p0 = 0.4",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.p5, main = "WRP Mesic Probabilities of Change - p0 = 0.5",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.p6, main = "WRP Mesic Probabilities of Change - p0 = 0.6",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.p7, main = "WRP Mesic Probabilities of Change - p0 = 0.7",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.p8, main = "WRP Mesic Probabilities of Change - p0 = 0.8",xlab = "Date", xaxlab = data.fill$year )
+#plot(results.bcp.p9, main = "WRP Mesic Probabilities of Change - p0 = 0.9",xlab = "Date", xaxlab = data.fill$year )
+
+#install.packages('Rbeast')
+library(Rbeast)
+
+results.beast = beast(data.fill$mesic, start = as.Date('2004-6-1'), deltat = "3 months", dump.ci = T) ##not quite right - missing 2016 data.... 
+plot(results.beast, interactive = F)
+
+## missing data
+results.beast.na = beast(as.numeric(data.ts.NA), start = as.Date('2004-1-1'), deltat = "3 months") 
+plot(results.beast.na, interactive = F) ## missing data leads to higher likelihood of negative slopes, and an extra breakpoint estimated in the trend
+
+## Parameters 
+
+## BSTS 
+library(CausalImpact)
+
+## Include PDSI as a predictor
+data.bsts=  cbind(data.fill$mesic, pdsi.no2016$pdsi)
 data.bsts.time =  zoo(cbind(data.fill$mesic, pdsi$pdsi), as.Date(data.fill$system.time_start,format = "%b %e, %Y")) ## includes missing data period
 
 ## define pre and post restoration periods
