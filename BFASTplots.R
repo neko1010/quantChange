4054758b6ef2dacc2034dc41bb4928a4f4b81907
Nick Kolarik <nicholaskolarik@u.boisestate.edu>
2025-02-21 11:59:25 -0700

    updated comments

--- BFASTplots.R
+++ BFASTplots.R
@@ -1,12 +1,9 @@
-
-
-
 ## BFAST MAPS
-#https://gis.stackexchange.com/questions/336092/can-bfast-algorithm-be-applied-to-to-a-whole-study-area-roi
 library(bfast)
 library(terra)
 library(gtools)
 
+## import .tif as raster
 ras = rast('Yankee.tif')
 plot(ras)
 crs(ras)
@@ -18,6 +15,7 @@ july19
 ## add this empty rast to the full stack
 ras = c(ras, july19)
 ras
+
 ## add time stamps to each
 names = names(ras)
 dates = sapply(names, function(name){
@@ -29,14 +27,13 @@ dates.sort = order(dates)
 dates.sorted = as.Date(dates[dates.sort])
 ras.sort = ras[[dates.sort]]
 
+## apply the sorted dates to the 'time' property of the raster
 terra::time(ras.sort)= dates.sorted
-
-## INSERT dates.sorted info to raster
 
 ## double check times
 time(ras.sort)
 
-
+## Function to apply the bfast function and return outputs of interest
 xbfast <- function(data) {  
   mesic <- ts(data, frequency=4, start=2004) 
   result <- bfast(mesic, season="harmonic", decomp = 'stlplus')#, max.iter=2, breaks=1)
@@ -58,56 +55,42 @@ xbfast <- function(data) {
   return(c(st_dif,bp,Magni,Timing)) 
 }
 
-#bfast.output <- app(ras.sort, fun=bfastfun)
+## apply bfast to every pixel using app()
 bfast.output <- app(ras.sort, fun=xbfast)
+## plot all outputs
 plot(bfast.output)
 
-## not sure why, but all of the above outputs are inverted (both NS and EW) - need to apply the flip() to correct
+## not sure why, but all of the above outputs are inverted (both NS and EW) -
+## need to apply the flip() to correct - double check whether this is necessary
 plot(flip(ras.sort[[64]]), #main = "Mesic vegetation proportion Sep 2020 flipped", 
-     range = c(0, 0.6), plg = list(title = "proportion"))
+     range = c(0, 0.6), plg = list(title = "proportion")) ## sanity check
 
+## a 4 panel plot
 parameter <- par(mfrow = c(1,4))
+## sep 2004
 parameter <- plot(flip(ras.sort[[4]]), main = "Panel A\nMesic vegetation\n proportion Sep 2004", range = c(0, 0.6))
-parameter <- plot(flip(ras.sort[[64]]), main = "Panel A\nMesic vegetation\n proportion Sep 2020", range = c(0, 0.6))
+## sep 2020
+parameter <- plot(flip(ras.sort[[64]]), main = "Panel B\nMesic vegetation\n proportion Sep 2020", range = c(0, 0.6))
 
+## Individual BFAST outputs
 diff <- subset(bfast.output, 1)
 time <- subset(bfast.output, 2)
 magn <- subset(bfast.output, 3)
 magn.time <- subset(bfast.output, 4)
 
+## Magnitude change
 parameter <- plot(flip(magn), main = "Panel C\nLargest magnitude\n of change in trend")
 
 ## substitute dates for indices
 years.vect = c(rep(2004,4), rep(2005,4), rep(2006,4), rep(2007,4), rep(2008,4),rep(2009,4), rep(2010,4),rep(2011,4),
                rep(2012,4),rep(2013,4),rep(2014,4),rep(2015,4),rep(2017,4),rep(2018,4),rep(2019,4),rep(2020,4))
-#magn.time.dates = subst(magn.time, from = c(1:64), to = as.Date(dates.sorted))
+## Time of greatest magnitude change
 magn.time.dates = subst(magn.time, from = c(1:64), to = years.vect)
 parameter <- plot(flip(magn.time.dates), main = "Panel D\nTime of largest magnitude\n of change in trend")
 
-### CK's date fix - single plots for each
-#library(tidyverse)
-##dates.sorted <- as.Date(dates[dates.sort])
-##magn.time.dates <- subst(magn.time, from=c(1:64), to=dates.sorted)
-#ggplot(as.data.frame(magn.time.dates, xy=TRUE), 
-#       aes(x=x, y=y, fill=as.Date(lyr.4))) +
-#  geom_raster() +
-#  scale_fill_viridis_c(name = "",
-#                       trans="date") +
-#  labs(title = "Time of largest magnitude of change in trend") +
-#  coord_sf(default_crs = crs(ras)) +
-#  theme_void() +
-#  theme(panel.border = element_rect(color="black", fill=NA))
-
-#writeRaster(diff, filename="season_diff.tif", format="GTiff") ## EMPTY - makes sense - no bp here
-#writeRaster(time, filename="season_diff_time.tif", format="GTiff") ##BORING
-#writeRaster(magn, filename="trend_mag.tif", format="GTiff") ##interesting
-#writeRaster(magn.time, filename="trend_mag_timing.tif", format="GTiff") ##interesting
-
-
-## Images reduced to a single value ##
+############ Images reduced to a single value for comparison ###################
 
 ## read the data
-#data = read.csv('BSU/MRRMAid/lab/quantChange/yankee.csv')
 data = read.csv('yankee.csv')
 
 ## BFAST can't handle missing values 
@@ -152,10 +135,6 @@ data.NAs = data.NAs[order(data.NAs$date),]
 ## create a time series object
 data.ts = ts(data.fill$mesic, frequency = 4)
 data.ts.NA = ts(data.NAs$mesic, frequency = 4)
-
-## assigning time units to observations
-#tsp(data.ts.NA) = c(as.Date("2004-06-01"),as.Date("2020-09-01"), 4 )
-
 
 plot(data.ts)
 plot(data.ts.NA)
